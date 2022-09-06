95f3939426df29776bd01c6adc3738032b091ad9
tommyhealth <kr_lizaa@mail.ru>
2022-09-06 17:25:38 +0300

    Initial commit

---
+++ src/main/java/ru/fallindawn/booking/BookingApplication.java
@@ -0,0 +1,14 @@
+package ru.fallindawn.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingApplication.class, args);
+	}
+
+
+}
