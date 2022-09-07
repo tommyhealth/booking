7f4707d23baac3cef69a7aa717b0d74a3628d43a
tommyhealth <kr_lizaa@mail.ru>
2022-09-07 15:17:01 +0300

    commit sql

---
+++ src/main/resources/sql/insert_data.sql
@@ -0,0 +1,10 @@
+insert into room(price, capacity, type_class)
+values (1800, 2, 'GRAND'),
+       (2100, 2, 'STANDARD'),
+       (1800, 1, 'STANDARD'),
+       (3100, 1, 'SUPERIOR'),
+       (3800, 2, 'SUPERIOR'),
+       (4100, 1, 'DELUXE'),
+       (4900, 2, 'DELUXE'),
+       (4200, 4, 'FAMILY'),
+       (3300, 3, 'TRIPLE')
---
+++ src/main/resources/sql/tables_init.sql
@@ -0,0 +1,18 @@
+CREATE TABLE client
+(
+    id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+    phone CHARACTER VARYING (20) NOT NULL,
+    name CHARACTER VARYING(100) NOT NULL,
+    second_name CHARACTER VARYING(100) NOT NULL
+);
+
+CREATE TABLE room
+(
+    id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+    client_id uuid REFERENCES client (id),
+    price DECIMAL NOT NULL,
+    capacity INTEGER NOT NULL,
+    type_class CHARACTER VARYING(100),
+    check_in DATE,
+    check_out DATE
+);
