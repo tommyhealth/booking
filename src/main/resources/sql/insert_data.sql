9e3f7bc3918fe72bd5e0c6b183608682e0b7198c
Елизавета <103944682+tommyhealth@users.noreply.github.com>
2022-09-08 16:20:23 +0300

    commit sql (#1)
    * commit sql
    
    * correction sql

---
+++ src/main/resources/sql/insert_data.sql
@@ -0,0 +1,10 @@
+insert into room(price, capacity, type_class)
+values (1800.00, 2, 'GRAND'),
+       (2100.25, 2, 'STANDARD'),
+       (1800.00, 1, 'STANDARD'),
+       (3100.355, 1, 'SUPERIOR'),
+       (3800.785, 2, 'SUPERIOR'),
+       (4100.00, 1, 'DELUXE'),
+       (4900.00, 2, 'DELUXE'),
+       (4200.245, 4, 'FAMILY'),
+       (3300.00, 3, 'TRIPLE')
